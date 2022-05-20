60e5b86f57862a86e6a7954f8f67a8536bf413ef
Marvin Joel Gonzalez Perez <mjoelgonzalez@unicesar.edu.co>
2022-05-20 18:30:45 -0500

    comit

--- src/App.js
+++ src/App.js
@@ -9,98 +9,104 @@ import cromenetro from './images/icon-detailed-records.svg';
 import { useState } from 'react';
 
 function App() {
- const [consulting,setConsulting]=useState(false);
- const [copyButtonMessage,setCopyButtonMessage]=useState("Copy");
- const [copyButtonColor,setCopyButtonColor]=useState("hsl(180, 66%, 49%)");
-  const verConsulta=()=>{
+  const [consulting, setConsulting] = useState(false);
+  const [copyButtonMessage, setCopyButtonMessage] = useState("Copy");
+  const [copyButtonColor, setCopyButtonColor] = useState("hsl(180, 66%, 49%)");
+  const verConsulta = () => {
     setCopyButtonMessage("Copy");
 
-    if(consulting==true){
+    if (consulting == true) {
       setConsulting(false);
-    }else{
+    } else {
       setConsulting(true);
     }
   }
-  const copyToClipBoard=()=> {
+  const copyToClipBoard = () => {
 
     var content = document.getElementById('textArea');
-    
+
     content.select();
     document.execCommand('copy');
 
     setCopyButtonMessage("Copied!");
-}
- console.log(consulting);
+  }
+  console.log(consulting);
   return (
-    <div className='constainer'>
 
-      <nav style={{ backgroundColor: "white", justifyItems: "center" }} >
-        <Navbar collapseOnSelect expand="lg">
-          <Container>
-            <Navbar.Brand href="#home"> <img src={images2} alt="lossgo" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Resources" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">Login</Nav.Link>
-                <button className='button-g'>Sign Up</button>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+    <>
+{/*parte superior*/}
+      <div style={{backgroundColor:"white"}}>
+        <nav style={{ backgroundColor: "white", justifyItems: "center" }} >
+          <Navbar collapseOnSelect expand="lg">
+            <Container>
+              <Navbar.Brand href="#home"> <img src={images2} alt="lossgo" /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  <NavDropdown title="Resources" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="#deets">Login</Nav.Link>
+                  <button className='button-g'>Sign Up</button>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
 
-      </nav>
+        </nav>
 
-      <div className='container' style={{ backgroundColor: "white" }}>
+        <div className='container' style={{ backgroundColor: "white" }}>
 
-        <div className='row align-items-center' style={{ "height": "600px" }}>
-          <div class='col-xs-12  col-md-6 md:top-50  ' style={{}}>
-            <h1>
-              More that just<br />
-              shorter links
-            </h1>
-            <p>
-              Build your brand´s recongnition and get detailed<br />
-              insights on how your links are performing.<br /><br />
-              <button className='button-g'>  Get Started  </button>
-            </p>
-          </div>
-          <div class='col-xs-12 col-md-6' style={{}}>
-            <img class="responsive" src={illustration_working} style={{ display: "flex", height: "auto" }} alt="logo" />
+          <div className='row align-items-center' style={{ "height": "600px" }}>
+            <div class='col-xs-12  col-md-6 md:top-50  ' style={{}}>
+              <h1>
+                More that just<br />
+                shorter links
+              </h1>
+              <p>
+                Build your brand´s recongnition and get detailed<br />
+                insights on how your links are performing.<br /><br />
+                <button className='button-g'>  Get Started  </button>
+              </p>
+            </div>
+            <div class='col-xs-12 col-md-6' style={{}}>
+              <img class="responsive" src={illustration_working} style={{ display: "flex", height: "auto" }} alt="logo" />
+            </div>
           </div>
         </div>
+
       </div>
 
-      <selection >
+{/*parte central*/}
+<div style={{backgroundColor:"hsl(0, 0%, 75%)"}}>
+    <selection >
 
         <p style={{ "background-color": "transparent", "height": "100px" }} />
         {
-          consulting? 
-          <>
-          <div class="container">
-          <div  style={{display:"flex",backgroundColor:"indigo", " border-radius":"15px"}}>
-             <p >We are learning to copy text to clipboard in JavaScript.</p>
-             <p id="textArea" style={{color:"hsl(180, 66%, 49%)"}}>We are learning to copy text to clipboard in JavaScript.</p>
-             <button className='button-n' onClick={copyToClipBoard}>{copyButtonMessage}</button>
-          </div>
-            
-          </div>
-            
-          </>
-         :<></>
+          consulting ?
+            <>
+              <div class="container">
+                <div style={{ display: "flex", backgroundColor: "indigo", " border-radius": "15%" }}>
+                  <p >We are learning to copy text to clipboard in JavaScript.</p>
+                  <p id="textArea" style={{ color: "hsl(180, 66%, 49%)" }}>We are learning to copy text to clipboard in JavaScript.</p>
+                  <button className='button-n' onClick={copyToClipBoard}>{copyButtonMessage}</button>
+                </div>
+
+              </div>
+
+            </>
+            : <></>
         }
 
-        <div style={{  backgroundColor: "transparent", "align-items": "center", "text-align": "center", justifyContent: "center", width: "100%" }}>
+        <div style={{ backgroundColor: "transparent", "align-items": "center", "text-align": "center", justifyContent: "center", width: "100%" }}>
           <h1 >
             Advanced Statistics
           </h1>
@@ -110,7 +116,7 @@ function App() {
         </div>
 
       </selection>
-{/*  <div style={{ "font-size": " 1vw",display:'flex', backgroundColor: "transparent", "align-items": "center", "text-align": "center", justifyContent: "center", width: "100%" }}>
+      {/*  <div style={{ "font-size": " 1vw",display:'flex', backgroundColor: "transparent", "align-items": "center", "text-align": "center", justifyContent: "center", width: "100%" }}>
           <h1 style={{ "font-size": " 3vw" }}>
             Advanced Statistics
           </h1>
@@ -118,41 +124,41 @@ function App() {
             Track how your links are performing across the web with our advanced statistics dashboard.
           </p>
         </div>*/}
-      <section style={{  }}>
+      <section style={{}}>
         <div className="container">
           <div className="row">
-            <div class='col-xs-1   col-md-3 ' style={{"background-color": "white", "align-items": "left", "justify-content": "left", "text-align": "left" }}>
-             <div  style={{ display: "flex", "justify-content": "center",     "align-items": "center", "background-position": "50% 50%","backgroundColor": "rgb(63, 46, 85)","border-radius": "50%", width:"80px",height:"80px"}}>
-               <img src={grafica} style={{width:"auto%" , height: "50%"}} alt="" />
-             </div> 
-             <h3>  <strong>Brand Recognition</strong></h3>
+            <div class='col-xs-1   col-md-3 ' style={{ "background-color": "white", "align-items": "left", "justify-content": "left", "text-align": "left" }}>
+              <div style={{ display: "flex", "justify-content": "center", "align-items": "center", "background-position": "50% 50%", "backgroundColor": "rgb(63, 46, 85)", "border-radius": "50%", width: "80px", height: "80px" }}>
+                <img src={grafica} style={{ width: "auto%", height: "50%" }} alt="" />
+              </div>
+              <h3>  <strong>Brand Recognition</strong></h3>
               <p>Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.  </p>
             </div>
             {/*linea*/}
-            <div class='col-xs-1   col-md-1 ' style={{ "background-color": "cyan",height:"10px","align-items": "center", "justify-content": "center", "text-align": "center" }}/>
+            <div class='col-xs-1   col-md-1 ' style={{ "background-color": "cyan", height: "10px", "align-items": "center", "justify-content": "center", "text-align": "center" }} />
             {/*linea*/}
-            <div class='col-xs-1  col-md-3' style={{"background-color": "white", "align-items": "left", "justify-content": "left", "text-align": "left" }}>
-            <div style={{ display: "flex", "justify-content": "center",     "align-items": "center",  "background-position": "50% 50%","backgroundColor": "rgb(63, 46, 85)","border-radius": "50%", width:"80px",height:"80px"}}>
-              <img src={cromenetro} style={{ width:"auto%" , height: "50%"}}  alt="" />
-            </div>
-              
-             <h3><strong>Detailed Records</strong></h3> 
+            <div class='col-xs-1  col-md-3' style={{ "background-color": "white", "align-items": "left", "justify-content": "left", "text-align": "left" }}>
+              <div style={{ display: "flex", "justify-content": "center", "align-items": "center", "background-position": "50% 50%", "backgroundColor": "rgb(63, 46, 85)", "border-radius": "50%", width: "80px", height: "80px" }}>
+                <img src={cromenetro} style={{ width: "auto%", height: "50%" }} alt="" />
+              </div>
+
+              <h3><strong>Detailed Records</strong></h3>
               <p>
-               Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.
+                Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.
               </p>
             </div>
             {/*linea*/}
-            <div class='col-xs-1   col-md-1 ' style={{"background-color": "cyan",height:"10px", "align-items": "center", "justify-content": "center", "text-align": "center" }}/>
+            <div class='col-xs-1   col-md-1 ' style={{ "background-color": "cyan", height: "10px", "align-items": "center", "justify-content": "center", "text-align": "center" }} />
             {/*linea*/}
-            
-            <div class='col-xs-1   col-md-3 ' style={{"background-color": "white", "align-items": "center", "justify-content": "center", "text-align": "left" }}>
-            <div style={{ display: "flex", "justify-content": "center",     "align-items": "center",  "background-position": "50% 50%","backgroundColor": "rgb(63, 46, 85)","border-radius": "50%", width:"80px",height:"80px"}}>
-              <img src={pinceles}style={{ width:"auto%" , height: "60%"}}  alt="" />
-            </div>
-              
+
+            <div class='col-xs-1   col-md-3 ' style={{ "background-color": "white", "align-items": "center", "justify-content": "center", "text-align": "left" }}>
+              <div style={{ display: "flex", "justify-content": "center", "align-items": "center", "background-position": "50% 50%", "backgroundColor": "rgb(63, 46, 85)", "border-radius": "50%", width: "80px", height: "80px" }}>
+                <img src={pinceles} style={{ width: "auto%", height: "60%" }} alt="" />
+              </div>
+
               <h3><strong>Fully Customizable</strong></h3>
               <p>
-              Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.
+                Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.
               </p>
             </div>
           </div>
@@ -161,29 +167,28 @@ function App() {
 
       <div className='container'>
         <div className='container' style={{ "position": "absolute", "top": "590px", }}>
-          <div class='lol' style={{ height:"150px","background-size": "100% 100%", backgroundColor: "rgb(63, 46, 85)", "border-radius": "5px" }} > <br></br>
-          <br></br>
-                <div className='row' style={{display: "flex", "align-items": "center", "justify-content": "center"}}>
-                  <div class='col-xs-12  col-md-8 ' style={{width:"70%"}}>
-                    <input className='kkkk' placeholder='horten a link here...' style={{}} required={{}}></input>
-                  </div><br />
-                  <div class='col-xs-12  col-md-2  ' style={{width:"15%"}}>
-                    <button variant="primary" onClick={verConsulta} className='button-n'>Shorten it!</button>
-                  </div>
-                </div>
-          
-                
+          <div class='lol' style={{ height: "150px", "background-size": "100% 100%", backgroundColor: "rgb(63, 46, 85)", "border-radius": "5px" }} > <br></br>
+            <br></br>
+            <div className='row' style={{ display: "flex", "align-items": "center", "justify-content": "center" }}>
+              <div class='col-xs-12  col-md-8 ' style={{ width: "70%" }}>
+                <input className='kkkk' placeholder='horten a link here...' style={{}} required={{}}></input>
+              </div><br />
+              <div class='col-xs-12  col-md-2  ' style={{ width: "15%" }}>
+                <button variant="primary" onClick={verConsulta} className='button-n'>Shorten it!</button>
+              </div>
+            </div>
 
-              <br></br>
+
+
+            <br></br>
           </div>
         </div>
       </div>
 
-      
+</div>
+    
+    </>
 
-
-
-    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   
   <React.StrictMode>
     <Apps/>
-<Footer/>
+    <Footer/>
   </React.StrictMode>
 );
 
